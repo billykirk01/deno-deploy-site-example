85036ecfabce43fef8e0fc58baa6f727ce35f843
Billy Kirk <kirk.william.c@gmail.com>
2022-06-22 15:26:05 -0500

    Updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,16 @@
+<script>
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        fetch("/api/time")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    });
+</script>
+
 <h1>Hello from Svelte and Deno!</h1>
